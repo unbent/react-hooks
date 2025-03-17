bc0ef64917a19ca96d9e14a5a94b1803621ac07b
unbent <daniel.polan@hotmail.com>
2025-03-17 16:08:44 -0400

    Exercise 06 - Extra Credit 02

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,34 +4,55 @@
 import * as React from 'react'
 import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
 
+const statusType = {
+  idle: 'idle',
+  pending: 'pending',
+  resolved: 'resolved',
+  rejected: 'rejected',
+}
+
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null);
+  const [status, setStatus] = React.useState(statusType.idle);
 
   React.useEffect(() => {
-    if (!pokemonName) return;
+    if (!pokemonName) {
+      setStatus(statusType.idle);
+      return;
+    }
 
+    setStatus(statusType.pending);
     setPokemon(null);
     fetchPokemon(pokemonName).then(
       pokemonData => {
         setPokemon(pokemonData);
+        setStatus(statusType.resolved);
       }
-    ).catch(error => setError(error));
+    ).catch(error => {
+      setError(error);
+      setStatus(statusType.rejected);
+    });
     
   }, [pokemonName])
 
-  if (error) return (
-    <div role="alert">
-      There was an error:{' '}
-      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-    </div>
-  )
-
-  if (pokemonName && !pokemon) return <PokemonInfoFallback name={pokemonName}/>
-
-  if (pokemon) return <PokemonDataView pokemon={pokemon}/>
-
-  return 'Submit a pokemon'
+  switch (status) {
+    case statusType.rejected:
+        return (
+        <div role="alert">
+          There was an error:{' '}
+          <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        </div>
+      )
+    case statusType.pending:
+      return <PokemonInfoFallback name={pokemonName}/>;
+    case statusType.resolved:
+      return <PokemonDataView pokemon={pokemon}/>
+    case statusType.idle:
+      return 'Submit a pokemon'
+    default:
+      throw new Error(`Error: Status "${status}" not recognized`);
+  }
 }
 
 function App() {
